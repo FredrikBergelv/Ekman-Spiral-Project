50069859659d93272b84b65b2794c3eb29df8c20
Fredrik Bergelv <fredrik.bergelv@live.se>
2026-05-28 14:59:06 +0200

    This push was done via a bash script

--- python/2layer_angle.py
+++ python/2layer_angle.py
@@ -1,3 +1,8 @@
+"""
+Created on Fri May 22 20:10:17 2026
+
+@author: fredrik
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -7,39 +12,99 @@ f = 1e-4  # Coriolis parameter
 U0 = 1.0  # Reference velocity
 
 def layer_dUdz0(phi, epsilon):
+      phi = phi/np.sqrt(2)
       R = (1 - epsilon) / (1 + epsilon)
       A1 = np.exp(phi)
       E1 = A1 * (np.cos(phi) + 1j * np.sin(phi))
       Z = (1 + 1j) * (E1**2 - R) / (E1**2 + R)
       return Z
+  
+def U(z_tilde, phi, epsilon):
+    "This solution is normalizd for h_1"
+
+    gamma = epsilon
+    den = 1 - gamma + np.exp(np.sqrt(2)*(1 + 1j)*phi) * (1 + gamma)
+    A1 = -U0 * ((1 - gamma) / den)
+    B1 = -U0 * ((1 + gamma) * np.exp(np.sqrt(2)*(1 + 1j)*phi) / den)
+    B2 = -U0 * (2* np.exp(((1 + 1j)/np.sqrt(2)) * phi * (1 + 1/gamma))/ den)
+
+    U1 =  U0 + A1 * np.exp((1 + 1j)*z_tilde/np.sqrt(2)) + B1 * np.exp(-(1 + 1j)*z_tilde/np.sqrt(2))
+    U2 =  U0 + B2 * np.exp(-(1 + 1j)*z_tilde/(np.sqrt(2)*gamma))
+
+    return np.where(z_tilde < phi, U1, U2)
+
+
+def ekman_transport(phi, epsilon, zmax=150.0, Nz=50000):
+    z = np.linspace(0, zmax, Nz)
+    U_vals = U(z, phi, epsilon)
+    M = np.trapezoid(U_vals-U0, z)
+    print(np.angle(M, deg=True), np.angle(M, deg=True)-np.angle(layer_dUdz0(phi, epsilon), deg=True))
+    return M
 
 
 # Plottin gparameters
-epsilons = [0.0001, 0.01, 0.05, 0.1, 0.5]
-phis = np.linspace(0,4,100000)
+extent = 5
 
+nu_ratios = np.array([0.00001, 0.1, 0.5, 1/0.5, 1/0.1, 1/0.00001])
+epsilons = np.sqrt(nu_ratios)
 
+phis = np.linspace(0,extent,100000)
 
-plt.figure(figsize=(8,5))
+#%%
+plt.figure(figsize=(8*extent/3,5))
 for epsilon in epsilons:
 
     layer_angle = np.angle(layer_dUdz0(phis, epsilon), deg=True)
 
-    plt.plot(phis, layer_angle, label=fr'$\epsilon={epsilon:.4f}$')
+    plt.plot(phis, layer_angle, label=fr'$\nu_2/\nu_1=={epsilon**2:.0e}$')
+
+
+plt.hlines(45, min(phis), max(phis), color="black", linestyle='--', label="45° reference")
+plt.vlines([np.pi/np.sqrt(2), np.sqrt(2)*np.pi], 5, 90, color="black", linestyle=":", label=r"$\pi/\sqrt{2}$ and $\sqrt{2}\pi$")
+
+plt.xlabel(r"Dimensionless lower layer thickness, $\varphi$ [-]",fontsize=11)
+plt.ylabel(r"Surface angle, $\theta$ [deg]",fontsize=11)
+plt.suptitle("Surface Angle for 2-layer Model", fontsize=14)
+plt.title("Surface angle vs lower layer thickness",fontsize=13)
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.legend(loc="lower right", fontsize=11)
+plt.ylim(0,95)
+plt.yticks([0,15,30,45,60,75,90])
+plt.xticks(np.arange(0, extent + 0.5, 0.5))
+save_name="2layer_angle"
+plt.savefig(f"plots/{save_name}.png", dpi=400)
+plt.savefig(f"../Ekman-Spirals-with-Variable-Eddy-Viscosity-Article/Figures/{save_name}.png", dpi=400)
+
+plt.show()
+
+#%%
+nu_ratios = np.array([0.00001, 0.1, 0.5, 1/0.5, 1/0.1, 1/0.00001])
+epsilons = np.sqrt(nu_ratios)
+
+plt.figure(figsize=(8*extent/3,5))
+for epsilon in epsilons:
+
+    M_vals = np.array([ekman_transport(phi, epsilon) for phi in phis])
+    trans_angle = np.angle(M_vals, deg=True)
+    plt.plot(phis, trans_angle, label=fr'$\nu_2/\nu_1={epsilon**2:.0e}$')
 
 # -------------------------
 # Reference lines
 # -------------------------
-plt.hlines(45, min(phis), max(phis), color="black", linestyle='--', label="45° reference")
-plt.vlines([np.pi/2, np.pi], 40, 90, color="black", linestyle=":", label=r"$\pi/2$ and $\pi$")
+plt.vlines([np.pi/np.sqrt(2), np.sqrt(2)*np.pi], 90, 185, color="black", linestyle=":", label=r"$\pi/\sqrt{2}$ and $\sqrt{2}\pi$")
 
-plt.xlabel(r"Lower layer thickness, $\varphi$ [-]")
-plt.ylabel(r"Surface angle, $\theta$ [deg]")
-plt.suptitle("Surface Angle for 2-layer Model", fontsize=14)
-plt.title("Surface angle vs layer thickness")
+plt.xlabel(r"Dimensionless lower layer thickness, $\varphi$ [-]",fontsize=11)
+plt.ylabel(r"Transport angle, $\theta_T$ [deg]", fontsize=11)
+plt.suptitle("Transport Angle for 2-layer Model", fontsize=14)
+plt.title("Transport angle vs lower layer thickness",fontsize=13)
 plt.grid(True, linestyle='--', alpha=0.6)
-plt.legend(fontsize=11)
-plt.ylim(37,93)
-plt.savefig("2layer_angle.png", dpi=400)
+plt.legend(loc="lower right", fontsize=11)
+plt.ylim(90,185)
+plt.yticks([90, 105, 120, 135, 150, 165, 180])
+plt.xticks(np.arange(0, extent + 0.5, 0.5))
+save_name="2layer_transport"
+plt.savefig(f"plots/{save_name}.png", dpi=400)
+plt.savefig(f"../Ekman-Spirals-with-Variable-Eddy-Viscosity-Article/Figures/{save_name}.png", dpi=400)
+
 plt.show()
 
--- python/exponential_angle.py
+++ python/exponential_angle.py
@@ -1,61 +1,104 @@
+"""
+Created on Fri May 22 20:10:17 2026
+
+@author: fredrik
+"""
 import numpy as np
 from scipy.special import kv
 import matplotlib.pyplot as plt
-from matplotlib.ticker import FuncFormatter
-from kelvinfunctions_ker_kei import ker0, kei0, ker1, kei1
 
 f = 1e-4 
 U0 = 1
-phi0 = 0.5
 k = 10 # do not need in reality 
 
-def old_sol(z, phi=phi0):
-      alpha = phi*2*np.sqrt(1j*f)
-      num = kv(0,alpha*np.exp(k*z/2))
-      denum = 2*kv(1,alpha)
-      ans = U0*k*alpha*np.exp(k*z)* num / denum
-      return ans 
+
+def U(z_tilde, phi):
+    "Nomrlaized z_tilde=zk"
+    
+    exp = np.exp(z_tilde/2)
+    alpha = 2 * phi * np.sqrt(1j)
+    num = exp*kv(1, alpha*exp)
+    den = kv(1, alpha)
+    ans = U0 * (1-num/den)
+    return ans
 
 
-def dUdz(z, phi=phi0):
+def dUdz(z, phi):
+    
+    alpha = 2 * phi * np.sqrt(1j)
+    num = kv(0, alpha * np.exp(k*z/2))
+    den = 2*kv(1, alpha)
+    ans = U0 * k * alpha * np.exp(k*z) * num /  den
 
-      arg1 = 2*phi*np.exp(k*z/2)
-      arg2 = 2*phi
-      num = ker0(arg1)+ 1j*kei0(arg1)
-      denum = ker1(arg2) + 1j*kei1(arg2)
-      ans = np.sqrt(1j)*U0*k*phi*np.exp(k*z)*(num /(denum) )
-      return ans
+    return ans
+
+def ekman_transport(phi, zmax=10.0, Nz=50000):
+    z = np.linspace(0, zmax, Nz)
+    U_vals = U(z, phi)
+    M = np.trapezoid(U_vals-U0, z)
+    return M
 
 def calculate_angle(dUdz):
     # Compute the angle of dU/dz
     theta = np.angle(dUdz, deg=True)
     return theta
 
+phis = np.logspace(-50, np.log10(3), 1000)
+
+#%%
 # ===============================
 # Plotting
 # ===============================
-
-phis = np.logspace(-5,0.6,1000)
-
-
 plt.figure(figsize=(8, 5))
 plt.suptitle("Surface Angle for Exponetial Model", fontsize=14)
-plt.title("Surface angle vs decayrate")
+plt.title("Surface angle vs layer thickness",fontsize=13)
 
 angles = np.angle(dUdz(0, phis), deg=True)
+
 plt.plot(phis, angles)
 
-plt.xlabel(r"Height ratio, $\varphi$ [-]")
-plt.ylabel(r"Surface angle, $\theta$ [deg]")
+plt.xlabel(r"Dimensionless layer thickness, $\varphi$ [-]",fontsize=11)
+plt.ylabel(r"Surface angle, $\theta$ [deg]",fontsize=11)
 plt.grid(True, linestyle='--', alpha=0.6)
 
 plt.hlines(45, min(phis), max(phis), color="black", linestyle='--', label="45° reference")
 plt.legend(fontsize=11)
-#plt.xscale("log")
-plt.gca().xaxis.set_major_formatter(
-    FuncFormatter(lambda x, _: f"{x:.5f}".rstrip('0').rstrip('.'))
-)
-plt.ylim(37,93)
-plt.savefig("exponential_angle.png", dpi=400)
+
+plt.ylim(0,95)
+plt.yticks([0, 15, 30, 45, 60, 75, 90])
+plt.xticks([0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0])
+
+save_name="exponential_angle"
+plt.savefig(f"plots/{save_name}.png", dpi=400)
+plt.savefig(f"../Ekman-Spirals-with-Variable-Eddy-Viscosity-Article/Figures/{save_name}.png", dpi=400)
+
 plt.show()
+
+
+#%%
+
+
+plt.figure(figsize=(8, 5))
+plt.suptitle("Transport Angle for Exponetial Model", fontsize=14)
+plt.title("Transport angle vs layer thickness",fontsize=13)
+
+Ms = np.array([ekman_transport(phi) for phi in phis])
+angles = np.angle(Ms, deg=True)
+
+plt.plot(phis, angles)
+
+plt.xlabel(r"Dimensionless layer thickness, $\varphi$ [-]",fontsize=11)
+plt.ylabel(r"Transport angle, $\theta_T$ [deg]",fontsize=11)
+plt.grid(True, linestyle='--', alpha=0.6)
+
+plt.ylim(90,185)
+plt.yticks([90, 105, 120, 135, 150, 165, 180])
+plt.xticks([0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0])
+
+save_name="exponential_transport"
+plt.savefig(f"plots/{save_name}.png", dpi=400)
+plt.savefig(f"../Ekman-Spirals-with-Variable-Eddy-Viscosity-Article/Figures/{save_name}.png", dpi=400)
+
+plt.show()
+
 
